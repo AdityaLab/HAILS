853778931dc943b127f4cdb171828542af3533e5
kage08 <harshavardhan864.hk@gmail.com>
2024-03-04 14:15:27 -0500

    Use torch dataset

--- src/ts_utils/datasets.py
+++ src/ts_utils/datasets.py
@@ -1,6 +1,5 @@
 from typing import Optional
 
-import numpy as np
 import torch
 from torch.utils.data import Dataset
 
@@ -8,27 +7,30 @@ from torch.utils.data import Dataset
 class TimeSeriesDataset(Dataset):
     def __init__(
         self,
-        data: np.ndarray,
+        data: torch.Tensor,
         horizon: int,
         lookback: int,
         forecast_start_idx: Optional[int],
+        scaled: bool = False,
         transform=None,
-        return_torch: bool = True,
     ):
         self.data = data
         self.horizon = horizon
         self.lookback = lookback
         self.forecast_start_idx = max(lookback, forecast_start_idx or 0)
         self.transform = transform
-        self.return_torch = return_torch
         self.X, self.Y = self._create_dataset()
+        self.scaled = scaled
 
     def _create_dataset(self):
+        self.means = self.data.mean(dim=0)
+        self.stds = self.data.std(dim=0)
+        self.maxs = self.data.max(dim=0).values
         X, Y = [], []
         for i in range(self.forecast_start_idx, len(self.data) - self.horizon + 1):
             X.append(self.data[i - self.lookback : i])
             Y.append(self.data[i : i + self.horizon])
-        return np.array(X), np.array(Y)
+        return torch.stack(X), torch.stack(Y)
 
     def __len__(self):
         return len(self.X)
@@ -37,23 +39,26 @@ class TimeSeriesDataset(Dataset):
         x, y = self.X[idx], self.Y[idx]
         if self.transform:
             x, y = self.transform(x), self.transform(y)
-        if self.return_torch:
-            x, y = torch.tensor(x), torch.tensor(y)
+        if self.scaled:
+            # x = (x - self.means) / self.stds
+            # y = (y - self.means) / self.stds
+            x = x / self.maxs
+            y = y / self.maxs
         return x, y
 
 
 class HierarchicalTimeSeriesDataset:
     def __init__(
         self,
-        data: np.ndarray,
+        data: torch.Tensor,
         horizon: int,
         lookback: int,
         forecast_start_idx: Optional[int],
-        h_matrix: np.ndarray,
+        h_matrix: torch.Tensor,
+        scaled: bool = True,
         transform=None,
-        return_torch: bool = True,
     ):
         self.time_series_dataset = TimeSeriesDataset(
-            data, horizon, lookback, forecast_start_idx, transform, return_torch
+            data, horizon, lookback, forecast_start_idx, scaled, transform
         )
         self.h_matrix = h_matrix
